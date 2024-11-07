c119db8446902bc286aa078e0032c9978d1423ae
zeplin78 <muffi3333@gmail.com>
2024-11-07 22:01:41 +0800

    updating backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
@@ -17,16 +18,16 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to database')
-    // listen to port
+    // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
+      console.log('connected to db & listening on port', process.env.PORT)
     })
   })
-  .catch((err) => {
-    console.log(err)
-  }) 
+  .catch((error) => {
+    console.log(error)
+  })
